95212690e8644a99732db9bd540dc4814d1e99f9
zhafir01 <adli_prayata_34rpl@student.smktelkom-mlg.sch.id>
2025-07-30 10:20:16 +0700

    Add files via upload

---
+++ Kelas_XR7.java
@@ -0,0 +1,23 @@
+public class Kelas_XR7 {
+    public static void main(String[] args) {
+        //tipe_data nama_variabel = nilai;
+        String nama = "Adli Zhafir Prayata";
+        int usia = 15;
+        String kelas = "XR7";
+        String asal_sekolah = "SMPN 3 PETERONGAN";
+        String hobi = "membaca dan olahraga badminton";
+        String nama_ayah = "Arif";
+        String nama_ibu = "Rina";
+        
+        System.out.println(nama);
+        System.out.println("Usia: " + usia + " tahun");
+        System.out.println("Kelas: " + kelas);
+        System.out.println("Asal Sekolah: " + asal_sekolah);
+        System.out.println("Hobi: " + hobi);
+        System.out.println("Nama Ayah: " + nama_ayah);
+        System.out.println("Nama Ibu: " + nama_ibu);
+
+    
+        
+    }
+}
